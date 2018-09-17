b52f61da3423b5cc0fe899d00d9f7f74f964c6ec
Sharmina Akhtar <sakht318@gmail.com>
2018-09-17 10:02:28 +0000

    automatically backed up by learn

---
+++ app/workers/songs_worker.rb
@@ -0,0 +1,10 @@
+class SongsWorker
+  require 'csv'
+  include Sidekiq::Worker
+
+  def perform(song_file)
+    CSV.foreach(song_file, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
+  end
+end
